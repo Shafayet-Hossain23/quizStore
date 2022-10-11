0bfdee9512b816e50cc0c2fd96719152d6d76c18
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-10-12 00:36:06 +0600

    create-topics-components

---
+++ src/Components/Subject/Subject.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Subject = ({ id, name, logo, total }) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Subject;
--- src/Components/Topics/Topics.js
+++ src/Components/Topics/Topics.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Subject from '../Subject/Subject';
 import './Topics.css'
 
 const Topics = () => {
+    const getData = useLoaderData();
+    const subjects = getData.data;
+    // console.log(subjects)
+
     return (
         <div>
             <div className='bg-image'>
 
+            </div>
+            <div className='subjects'>
+                {
+                    subjects.map(subject => <Subject
+                        key={subject.id}
+                        subject={subject}
+                    ></Subject>)
+                }
             </div>
         </div>
     );
