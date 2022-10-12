7ba2f0c5a3e1f1ce8a3a13c81454ae3a7cb3716f
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-10-12 08:08:19 +0600

    topic-section

--- src/Components/Subject/Subject.js
+++ src/Components/Subject/Subject.js
@@ -1,9 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import './Subject.css';
+import { faCoffee, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-const Subject = ({ id, name, logo, total }) => {
+const Subject = ({ subject }) => {
+    const { id, name, logo, total } = subject
     return (
-        <div>
-
+        <div className='subject-container'>
+            <div className="subject-img">
+                <img src={logo} alt="" />
+            </div>
+            <div className='subject-info' >
+                <h3>{name}</h3>
+                <p>Total Quiz: {total}</p>
+                <button className='btn-practice'>
+                    Start Practice <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+                </button>
+            </div>
         </div>
     );
 };
