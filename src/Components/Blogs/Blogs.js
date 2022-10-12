1aca02e06998ce0a3ed57a73c32240700b204c97
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-10-12 16:19:28 +0600

    blog-sectin

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>This is blog page</h1>
+        <div className='blogs-container'>
+            <div className='blog-question'>
+                <h3>What is the purpose of React Router?</h3>
+                <p>ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.
+                    It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+
+                </p>
+            </div>
+            <div className='blog-question'>
+                <h3>How does Context API work?</h3>
+                <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+            </div>
+            <div className='blog-question'>
+                <h3>What is purpose of useRef hook?</h3>
+                <p>The useRef returns a mutable ref object. This object has a property called .current. The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The .current property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component. <br />
+
+                    syntax: const refContainer = useRef(initialValue); </p>
+            </div>
+
         </div>
     );
 };
