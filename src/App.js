2a8edc9d51531808ef32b8b8408920f829f333cb
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-10-12 08:24:32 +0600

    dynamic-url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Blogs from './Components/Blogs/Blogs';
 import Error from './Components/Error/Error';
+import QuizQuestion from './Components/QuizQuestion/QuizQuestion';
 import Statistics from './Components/statistics/Statistics';
 import Topics from './Components/Topics/Topics';
 import Main from './Layout/Main';
@@ -26,6 +27,13 @@ function App() {
             return fetch('https://openapi.programming-hero.com/api/quiz')
           },
           element: <Topics></Topics>
+        },
+        {
+          path: '/topic/:topicId',
+          loader: ({ params }) => {
+            return fetch(`https://openapi.programming-hero.com/api/quiz/${params.topicId}`)
+          },
+          element: <QuizQuestion></QuizQuestion>
         },
         {
           path: '/statistics',
--- src/Components/Subject/Subject.js
+++ src/Components/Subject/Subject.js
@@ -2,9 +2,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Subject.css';
 import { faCoffee, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom';
 
 const Subject = ({ subject }) => {
     const { id, name, logo, total } = subject
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+        navigate(`/topic/${id}`)
+    }
     return (
         <div className='subject-container'>
             <div className="subject-img">
@@ -13,7 +18,7 @@ const Subject = ({ subject }) => {
             <div className='subject-info' >
                 <h3>{name}</h3>
                 <p>Total Quiz: {total}</p>
-                <button className='btn-practice'>
+                <button onClick={handleNavigate} className='btn-practice'>
                     Start Practice <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
                 </button>
             </div>
